c1b2d715a497fb62be15dd96992e6f528272222a
Aditya-0911 <aditya0911das@gmail.com>
2026-03-07 16:29:12 +0530

    Updated Resnet Transfer learning

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torchvision import models
 
 class BaselineCNN(nn.Module):
 
@@ -49,6 +50,51 @@ class BaselineCNN(nn.Module):
 
     def forward(self,x):
         x = self.features(x)
+        x = self.pool(x)
+        x = self.classifier(x)
+        return x
+    
+
+class ResnetTransfer(nn.Module):
+
+    def __init__(self,num_classes=10, freeze_backbone=True):
+        super().__init__()
+
+        resnet = models.resnet18(weights="IMAGENET1K_V1")
+
+        self.backbone = nn.Sequential(*list(resnet.children())[:-2]) # remove avgpool and fc layers
+
+        if freeze_backbone:
+            for param in self.backbone.parameters():
+                param.requires_grad = False
+
+        #CNN Head
+
+        self.cnn_head = nn.Sequential(
+            nn.Conv2d(512,256,3,padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(256,128,3,padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+        )
+
+        self.pool = nn.AdaptiveAvgPool2d((1,1))
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128,128),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(128,num_classes)
+        )
+
+
+    def forward(self,x):
+        x = self.backbone(x)
+        x = self.cnn_head(x)
         x = self.pool(x)
         x = self.classifier(x)
         return x
